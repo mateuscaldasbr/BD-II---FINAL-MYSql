4eb31916c56a42b0d2c37867a600dfb7da987110
mateuscaldasbr <pessoal@mateuscaldas.com.br>
2021-06-26 16:21:58 -0300

    teste insert aluno beta 01

--- insert-aluno.php
+++ insert-aluno.php
@@ -1,40 +1,45 @@
-<?php 
-    $servidor = 'mysql742.umbler.com';
-    $usuario = 'mateus';
-    $senha = 'kHDH9V44rwJL53M';
-    $banco = 'bdacademico';
+<?php
+include('conexaoBD.php');
+
+if (isset($_POST['cadastrar'])) {
+
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+
+    $query = mysqli_query($con, "INSERT INTO Aluno (CPF, nome) VALUES ('$cpf', '$nome')");
     
-    $con = mysqli_connect($servidor, $usuario, $senha, $banco);     
-
-        if (isset ($_POST['cadastrar'])){
-        
-        $nome = $_POST['nome'];
-        $cpf = $_POST['cpf'];
-
-        $query = mysqli_query($con, "INSERT INTO Aluno (CPF, nome) VALUES ('$cpf', '$nome')");
+    if ($query){
+        echo "Sucesso";
     }
+    else {
+        echo "Erro ao inserir os dados";
+    }
+}
 ?>
 
 <html>
-    <head>
-        <title>INSERT ALUNO</title>
-    </head>
-    <body>
-        <form>
-            <label>CPF</label><br/>
-            <input type="text" name="cpf"><br/>
-            
-            <label>Nome</label><br/>
-            <input type="text" name="nome"><br/>
 
-            <select>
-                <option>Aluno</option>
-                <option>Professor</option>
-            </select>
+<head>
+    <title>INSERT ALUNO</title>
+</head>
 
-            <br/>
+<body>
+    <form>
+        <label>CPF</label><br />
+        <input type="text" name="cpf"><br />
 
-            <button name="cadastrar">CADASTRAR</button>
-        </form>
-    </body>
+        <label>Nome</label><br />
+        <input type="text" name="nome"><br />
+
+        <select>
+            <option>Aluno</option>
+            <option>Professor</option>
+        </select>
+
+        <br />
+
+        <button name="cadastrar">CADASTRAR</button>
+    </form>
+</body>
+
 </html>
