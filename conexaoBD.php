43b530a6b6a04bb2bca6ef65dd8335da23c24879
mateuscaldasbr <pessoal@mateuscaldas.com.br>
2021-06-26 16:43:57 -0300

    CONEXÃO SUCESSO

--- conexaoBD.php
+++ conexaoBD.php
@@ -4,10 +4,4 @@ $usuario = 'mateus';
 $senha = 'kHDH9V44rwJL53M';
 $banco = 'bdacademico';
 
-$con = mysqli_connect($servidor, $usuario, $senha, $banco);
-if ($con->connect_error) {
-die("Connection failed: " . $con->connect_error);
-}else{
-echo "Connect Success";
-}
-?>
+$con = mysqli_connect($servidor, $usuario, $senha, $banco) or die ("Erro ao conectar no Banco de Dados");
