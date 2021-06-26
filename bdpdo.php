7a90bbd46d5ac03e69d4d1a34de58f5abf9051eb
mateuscaldasbr <pessoal@mateuscaldas.com.br>
2021-06-26 15:34:49 -0300

    try cat pdo

--- bdpdo.php
+++ bdpdo.php
@@ -1,9 +1,16 @@
 <?php
 
-$dsn = 'mysql:host=mysql742.umbler.com,dbname=bdacademico';
+$dsn = 'mysql:host=mysql742.umbler.com;dbname=bdacademico';
 $username = 'mateus';
 $password = 'kHDH9V44rwJL53M';
 
-$pdo = new PDO($dsn. $username, $password);
+try{
+
+    $pdo = new PDO($dsn. $username, $password);
+
+} catch (PDOException $ex) {
+    echo 'Error: '.$ex->getMessage();
+}
+
 
 ?>
