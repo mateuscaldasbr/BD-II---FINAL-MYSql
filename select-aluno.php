d824bc2a11eefd5b11aafbe8aca4f73222224136
mateuscaldasbr <pessoal@mateuscaldas.com.br>
2021-06-26 23:57:24 -0300

    SELECT ALUNO OK

--- select-aluno.php
+++ select-aluno.php
@@ -1,12 +1,16 @@
-<?php
-include("connectbd.php");
-?>
-
 <html>
 <header>
     <style>
-        table {border: 1px solid #000;}
-        table td {border: 1px solid #000;}
+        table {
+            border: 1px solid #000;
+            border-spacing: 0px;
+        }
+
+        table td,
+        th {
+            border: 1px solid #000;
+            padding: 5px;
+        }
     </style>
 </header>
 
@@ -20,11 +24,18 @@ include("connectbd.php");
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>MATEUS CALDAS</td>
-                <td>12345678900</td>
-            </tr>
+            <?php
+            include("connectbd.php");
+            $resultado = mysqli_query($conn, "SELECT * FROM Aluno");
+            while ($linha = mysqli_fetch_array($resultado)) {
+            ?>
+                <tr>
+                    <td><?php echo $linha['matricula']; ?></td>
+                    <td><?php echo $linha['nome']; ?></td>
+                    <td><?php echo $linha['CPF']; ?></td>
+                <?php }
+                ?>
+                </tr>
         </tbody>
     </table>
 </body>
